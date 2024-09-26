d486ecb24c5cf972457820619381814befccfe1f
LAPTOP-O3VOI188\alexp <alexperse7@hotmail.com>
2024-09-27 01:19:28 +0200

    ejercicios hechos en clase 27/09/2024 operaciones y expresiones

--- ejercicios_Alex/src/main/java/org/example/Main.java
+++ ejercicios_Alex/src/main/java/org/example/Main.java
@@ -6,43 +6,11 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("¿Como te llamas?");
 
-        Scanner entrada = new Scanner(System.in);
-        //system.in
+        Operadores operadores= new Operadores();
+        operadores.operators();
 
-        String nombre = entrada.next();
-
-        System.out.println("¿ A que curso vas ?");
-        String curso = entrada.next();
-
-        System.out.println("hola + nombre + bienvenido a " + curso);
-
-        int num1 = 0;
-        int num2 = 0;
-while (num1==num2){
-        System.out.println("Introduce un numero...");
-        num1 = entrada.nextInt();
-
-        System.out.println("Introduce un numero...");
-        num2 = entrada.nextInt();
-
-        if (num1==num2){
-            System.out.println("ERROR. LOS NUMEROS SON IGUALES.");
         }
 
         }
 
-        int resultado = num1 + num2;
-
-        System.out.println("el resultado de la suma es" + resultado);
-
-
-        ejercicios ejercicios_inicial = new ejercicios ();
-        ejercicios.actividad_inicial();
-
-    }
-
-}
